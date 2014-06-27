ecaefdeb631ed6e82c658e5bad77a598c3c67e64
Peter Heiner <p.heiner@gmail.com>
2014-06-27 16:35:58 +0100

    add model for entries

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=64)
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
+    published = models.BooleanField()
+    author = models.ForeignKey(User)
+    body = models.TextField()
+ 
+    def __str__(self):
+        return "%s (%s)" % (self.title, self.author.name)
+
