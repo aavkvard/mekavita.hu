767df0db39c4d21b669ff0c10e6b8d952946aa97
root <aavkvard@gmail.com>
2014-06-29 23:09:21 +0000

    add index view primitive

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from blog.models import Entry
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_entries'
+
+    def get_queryset(self):
+        return Entry.objects.order_by('-modified')[:10]
+
+
+class DetailView(generic.DetailView):
+    model = Entry
+    template_name = 'blog/detail.html'
+
+def index(request):
+    latest_entries = Entry.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_entries': latest_entries}
+    return render(request, 'blog/index.html', context)
--- mekavita_hu/urls.py
+++ mekavita_hu/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mekavita_hu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
