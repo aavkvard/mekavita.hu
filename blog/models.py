d223a3d1f2f05756a22787bfaf079f20db2e3627
root <aavkvard@gmail.com>
2014-06-30 05:37:25 +0000

    fix typo

--- blog/models.py
+++ blog/models.py
@@ -13,5 +13,5 @@ class Entry(models.Model):
         verbose_name_plural = "entries"
  
     def __str__(self):
-        return "%s (%s)" % (self.title, self.author.name)
+        return "%s (%s)" % (self.title, self.author.username)
 
